62fd4bb4f516a15ea20b4d73f869500dc999b473
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-08-28 01:43:15 +0100

    add: support for simple ron.yaml file.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/melbahja/ron
 
 go 1.13
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/urfave/cli/v2 v2.2.0
+	gopkg.in/yaml.v2 v2.2.2
+)
--- ron.go
+++ ron.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/urfave/cli/v2"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-)
 
-type (
-	Command struct {
-		Cmd  string
-		Args []string
-	}
+	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v2"
 )
 
 var (
@@ -56,61 +52,108 @@ func main() {
 func handle(c *cli.Context) error {
 
 	var (
-		err error
-		cmd *Command
-		dir string = fmt.Sprintf("%s/.ron", c.String("dir"))
+		err     error
+		cmds    []string
+		ronFile string = fmt.Sprintf("%s/ron.yaml", c.String("dir"))
 	)
 
-	_, err = os.Stat(dir)
-	check(err)
+	if _, err = os.Stat(ronFile); err == nil && c.Args().Len() == 1 {
 
-	cmd, err = getCmd(dir, c.Args().Slice())
-	check(err)
+		cmds, _ = getCmdFromRonFile(ronFile, c.Args().Get(0))
+	}
+
+	if cmds == nil {
+
+		dir := fmt.Sprintf("%s/.ron", c.String("dir"))
+
+		_, err = os.Stat(dir)
+		check(err)
+
+		cmd, err := getCmd(dir, c.Args().Slice())
+		check(err)
+		cmds = append(cmds, cmd)
+	}
 
 	if c.Bool("dry") {
 
-		fmt.Printf("will run: %s %s\n", cmd.Cmd, strings.Join(cmd.Args, " "))
+		fmt.Println("This will run:")
+		for cmd := range cmds {
+			fmt.Printf(" - %s\n", cmds[cmd])
+		}
+
 		return nil
 	}
 
-	check(run(c.String("dir"), cmd))
+	for cmd := range cmds {
+		check(run(c.String("dir"), cmds[cmd]))
+	}
 
 	return nil
 }
 
-func getCmd(dir string, args []string) (*Command, error) {
+func getCmd(dir string, args []string) (cmd string, err error) {
 
-	var (
-		err  error
-		stat os.FileInfo
-		cmd  = Command{
-			Cmd: dir,
-		}
-	)
+	var stat os.FileInfo
+
+	cmd = dir
 
 	for i, arg := range args {
 
 		if arg[0] == 0x2d {
-			cmd.Args = args[i:]
+			cmd += strings.Join(args[i:], " ")
 			break
 		}
 
-		cmd.Cmd += fmt.Sprintf("/%s", arg)
+		cmd += fmt.Sprintf("/%s", arg)
 	}
 
-	if stat, err = os.Stat(cmd.Cmd); err != nil {
-		return nil, err
+	if stat, err = os.Stat(cmd); err != nil {
+		return
 	}
 
 	if stat.IsDir() {
 
-		cmd.Cmd = fmt.Sprintf("%s/.default", cmd.Cmd)
-		if _, err = os.Stat(cmd.Cmd); err != nil {
-			return nil, err
+		cmd = fmt.Sprintf("%s/.default", cmd)
+
+		if _, err = os.Stat(cmd); err != nil {
+			return
 		}
 	}
 
-	return &cmd, nil
+	return cmd, nil
+}
+
+func getCmdFromRonFile(name, cmd string) ([]string, error) {
+
+	commands, err := parseRonFile(name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if cmds, ok := commands[cmd]; ok {
+
+		return cmds, nil
+	}
+
+	return nil, fmt.Errorf("Could not find: %s", cmd)
+}
+
+func parseRonFile(name string) (map[string][]string, error) {
+
+	data, err := ioutil.ReadFile(name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	commands := make(map[string][]string)
+
+	if err = yaml.Unmarshal([]byte(data), &commands); err != nil {
+		return nil, err
+	}
+
+	return commands, nil
 }
 
 func complete(c *cli.Context) {
@@ -121,16 +164,33 @@ func complete(c *cli.Context) {
 		dir += "/"
 	}
 
-	files, err := filepath.Glob(fmt.Sprintf("%s*", dir))
+	options, err := filepath.Glob(fmt.Sprintf("%s*", dir))
 	check(err)
 
-	for _, f := range files {
+	if c.Args().Len() < 1 {
+
+		ronFile := filepath.Join(c.String("dir"), "ron.yaml")
+
+		if _, err := os.Stat(ronFile); err == nil {
+
+			if commands, err := parseRonFile(ronFile); err == nil {
+
+				cmds := make([]string, 0, len(commands))
+				for k := range commands {
+					cmds = append(cmds, k)
+				}
+				options = append(options, cmds...)
+			}
+		}
+	}
+
+	for _, f := range options {
 		fmt.Println(strings.Split(strings.Replace(f, dir, "", 1), "/")[0])
 	}
 }
 
-func run(dir string, c *Command) error {
-	cmd := exec.Command(c.Cmd, c.Args...)
+func run(dir string, c string) error {
+	cmd := exec.Command("bash", "-c", c)
 	cmd.Env = os.Environ()
 	cmd.Dir = dir
 	cmd.Stdin = os.Stdin
